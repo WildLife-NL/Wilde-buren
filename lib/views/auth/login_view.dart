60e7fdac89fd0a560d2b5de6da817bcb8cb2e049
Janne van Seggelen <vanseggelenjanne@gmail.com>
2024-12-12 10:28:52 +0100

    Update Login and Verification UI

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -1,80 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:wilde_buren/config/app_config.dart';
-import 'package:wilde_buren/config/theme/custom_theme.dart';
+import 'package:wilde_buren/config/theme/custom_colors.dart';
 import 'package:wilde_buren/views/auth/verification_view.dart';
 import 'package:wilde_buren/widgets/custom_scaffold.dart';
 import 'package:wildlife_api_connection/auth_api.dart';
 
 class LoginView extends StatefulWidget {
-  const LoginView({super.key});
+  const LoginView({
+    super.key,
+  });
 
   @override
-  State<LoginView> createState() => _LoginViewState();
+  LoginViewState createState() => LoginViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class LoginViewState extends State<LoginView> {
   final _formKey = GlobalKey<FormState>();
+
   final TextEditingController _emailController = TextEditingController();
-  login() async {
-    if (_formKey.currentState!.validate()) {
-      final email = _emailController.text;
 
-      AuthApi(AppConfig.shared.apiClient).authenticate('Wilde buren', email);
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => VerificationView(
-            email: email,
-          ),
-        ),
-      );
-    }
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-        body: Form(
-      key: _formKey,
-      child: ListView(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
         children: [
-          const SizedBox(height: 50),
-          TextFormField(
-            controller: _emailController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Email*',
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Field cannot be empty.';
-              }
-
-              String pattern = r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}$';
-              RegExp regex = RegExp(pattern);
-
-              if (!regex.hasMatch(value)) {
-                return 'Give a valid email address.';
-              }
-              return null;
-            },
+          const SizedBox(height: 100),
+          Image.asset(
+            'assets/images/wildlife-logo.png',
+            fit: BoxFit.cover,
           ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: login,
-            child: Text(
-              'Inloggen',
-              style: CustomTheme(context).themeData.textTheme.titleSmall,
+          const SizedBox(height: 30),
+          const Text(
+            "Welkom bij Wilde Buren!",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: CustomColors.primary,
             ),
           ),
+          const Text(
+            "Login in met je email.",
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email*',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Field cannot be empty.';
+                    }
+
+                    String pattern = r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}$';
+                    RegExp regex = RegExp(pattern);
+
+                    if (!regex.hasMatch(value)) {
+                      return 'Give a valid email address.';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10),
+                MaterialButton(
+                  minWidth: double.maxFinite,
+                  color: CustomColors.primary,
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      final email = _emailController.text;
+                      await AuthApi(AppConfig.shared.apiClient)
+                          .authenticate('Wilde buren', email);
+
+                      if (!context.mounted) return;
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => VerificationView(
+                            email: email,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text(
+                    'Inloggen',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
-    ));
+    );
   }
 }
--- lib/views/auth/verification_view.dart
+++ lib/views/auth/verification_view.dart
@@ -1,20 +1,26 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:wilde_buren/app.dart';
 import 'package:wilde_buren/config/app_config.dart';
 import 'package:wilde_buren/config/theme/custom_colors.dart';
-import 'package:wilde_buren/views/onboarding/onboarding_view.dart';
 import 'package:wilde_buren/widgets/custom_scaffold.dart';
 import 'package:wildlife_api_connection/auth_api.dart';
 import 'package:wildlife_api_connection/models/user.dart';
 
 class VerificationView extends StatefulWidget {
-  const VerificationView({super.key, required this.email});
   final String email;
+
+  const VerificationView({
+    super.key,
+    required this.email,
+  });
+
   @override
-  State<VerificationView> createState() => _VerificationViewState();
+  VerificationViewState createState() => VerificationViewState();
 }
 
-class _VerificationViewState extends State<VerificationView> {
+class VerificationViewState extends State<VerificationView> {
   final FocusNode pincodeFocusNode = FocusNode();
   final TextEditingController _verificationCodeController =
       TextEditingController();
@@ -34,7 +40,7 @@ class _VerificationViewState extends State<VerificationView> {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          builder: (context) => const OnboardingView(),
+          builder: (context) => const Initializer(),
         ),
       );
     } catch (e) {
@@ -45,13 +51,48 @@ class _VerificationViewState extends State<VerificationView> {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-      automaticallyImplyLeading: true,
-      body: ListView(
+      body: Column(
         children: [
-          const SizedBox(
-            height: 50,
+          Container(
+            width: 35,
+            height: 35,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: CustomColors.primary,
+            ),
           ),
-          const SizedBox(height: 50),
+          const SizedBox(height: 20),
+          const Text(
+            'Email verificatie',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: CustomColors.primary,
+            ),
+          ),
+          const SizedBox(height: 30),
+          RichText(
+            text: TextSpan(
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+              children: [
+                const TextSpan(
+                  text:
+                      'We hebben u een mail gestuurd met een 6 cijferige code erin om te verifiëren dat dit email van u is: ',
+                ),
+                TextSpan(
+                  text: widget.email,
+                  style: const TextStyle(
+                    color: CustomColors.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 24),
           PinCodeTextField(
             pinTheme: PinTheme(
               shape: PinCodeFieldShape.box,
@@ -69,6 +110,36 @@ class _VerificationViewState extends State<VerificationView> {
             onCompleted: (p0) {
               checkVerificationCode();
             },
+          ),
+          const SizedBox(height: 24),
+          RichText(
+            text: TextSpan(
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+              children: [
+                const TextSpan(
+                  text: 'Heeft u geen mail ontvangen? klik dan ',
+                ),
+                TextSpan(
+                  text: "hier",
+                  style: const TextStyle(
+                    color: CustomColors.primary,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () async {
+                      await AuthApi(AppConfig.shared.apiClient)
+                          .authenticate('Wilde buren', widget.email);
+                    },
+                ),
+                const TextSpan(
+                  text: ' om een nieuwe mail te ontvangen.',
+                ),
+              ],
+            ),
           ),
         ],
       ),
